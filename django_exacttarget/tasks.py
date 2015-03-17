aee01a0c3aab70919917f3e38fb482cc5830d1c1
Mike Lewis <mlewis@sandow.com>
2015-03-17 11:37:42 -0400

    Add task for removing subscribers

--- django_exacttarget/tasks.py
+++ django_exacttarget/tasks.py
@@ -1,9 +1,14 @@
 from .client import ETClient
 
-from celery import shared_task
+from celery import task
 
 
-@shared_task
+@task(name='django_exacttarget.tasks.add_subscriber')
 def add_subscriber(email, properties):
     client = ETClient()
     client.add_subscriber(email, **properties)
+
+@task(name='django_exacttarget.tasks.remove_subscriber_from_lists')
+def remove_subscriber_from_lists(email, list_ids):
+    client = ETClient()
+    client.remove_subscriber_from_lists(email, list_ids)
