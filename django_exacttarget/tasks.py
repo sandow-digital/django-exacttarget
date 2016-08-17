71a11580f423dfa1c992cc4ef914c76274ba357a
Mike Lewis <mlewis@sandow.com>
2016-08-17 11:41:00 -0400

    Fix double optin sub call

--- django_exacttarget/tasks.py
+++ django_exacttarget/tasks.py
@@ -27,7 +27,7 @@ def add_dataextension_row(name, properties, client_id=None, client_secret=None):
 @task(name='django_exacttarget.tasks.add_subscriber_doubleoptin')
 def add_subscriber_doubleoptin(email, properties=None, client_id=None, client_secret=None):
     # check if email already added in account
-    subscriber = Subscriber(email=email)
+    subscriber = Subscriber(email=email, client_id=client_id, client_secret=client_secret)
     sub_resp = subscriber.fetch()
     if not sub_resp.results:
         # New subscriber, add as unsubed to support double opt-in
@@ -39,7 +39,7 @@ def add_subscriber_doubleoptin(email, properties=None, client_id=None, client_se
 
 @task(name='django_exacttarget.tasks.add_subscriber_doubleoptin_confirm')
 def add_subscriber_doubleoptin_confirm(email, properties=None, client_id=None, client_secret=None):
-    subscriber = Subscriber(email=email)
+    subscriber = Subscriber(email=email, client_id=client_id, client_secret=client_secret)
     sub_resp = subscriber.fetch()
     if sub_resp.results:
         sub_resp = subscriber.save({'Status': 'Active'})
