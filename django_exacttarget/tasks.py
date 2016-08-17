4bf8b587edde88c3751b6253d8484e9bd50a8cb7
Mike Lewis <mlewis@sandow.com>
2016-08-17 11:02:05 -0400

    Adding double optin tasks

--- django_exacttarget/tasks.py
+++ django_exacttarget/tasks.py
@@ -24,5 +24,22 @@ def add_dataextension_row(name, properties, client_id=None, client_secret=None):
     de = DataExtension(name=name, client_id=client_id, client_secret=client_secret)
     de.add_row(properties)
 
-#@task(name='django_exacttarget.tasks.add_subscriber_doubleoptin')
-#def add_subscriber_doubleoptin(email, properties=None, client_id=None, client_secret=None):
+@task(name='django_exacttarget.tasks.add_subscriber_doubleoptin')
+def add_subscriber_doubleoptin(email, properties=None, client_id=None, client_secret=None):
+    # check if email already added in account
+    subscriber = Subscriber(email=email)
+    sub_resp = subscriber.fetch()
+    if not sub_resp.results:
+        # New subscriber, add as unsubed to support double opt-in
+        if properties:
+            properties.update({'Status': 'Unsubscribed'})
+        else:
+            properties = {'Status': 'Unsubscribed'}
+        sub_resp = subscriber.save(properties)
+
+@task(name='django_exacttarget.tasks.add_subscriber_doubleoptin_confirm')
+def add_subscriber_doubleoptin_confirm(email, properties=None, client_id=None, client_secret=None):
+    subscriber = Subscriber(email=email)
+    sub_resp = subscriber.fetch()
+    if sub_resp.results:
+        sub_resp = subscriber.save({'Status': 'Active'})
